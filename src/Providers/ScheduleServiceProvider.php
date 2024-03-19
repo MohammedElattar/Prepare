13d6e7822322034cc8b7c854fbaab7f2c88586f8
Mohamed Attar <mohammedattar0100020@gmail.com>
2024-03-19 11:08:47 +0200

    Update schedule list

--- src/Providers/ScheduleServiceProvider.php
+++ src/Providers/ScheduleServiceProvider.php
@@ -9,13 +9,13 @@ class ScheduleServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->app->booted(function () {
-                $schedule = $this->app->make(Schedule::class);
-                $schedule->command('backup:clean --only-db --disable-notifications')->daily()->at('01:00');
-                $schedule->command('backup:run --only-db --disable-notifications')->daily()->at('01:30');
-            });
-        }
+        // if ($this->app->runningInConsole()) {
+        //     $this->app->booted(function () {
+        //         $schedule = $this->app->make(Schedule::class);
+        //         $schedule->command('backup:clean --only-db --disable-notifications')->daily()->at('01:00');
+        //         $schedule->command('backup:run --only-db --disable-notifications')->daily()->at('01:30');
+        //     });
+        // }
     }
 
     public function register()
