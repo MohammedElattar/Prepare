17b22473cd385d8e6bdde2c4891646dfa31e2221
Mohamed Attar <mohammedattar0100020@gmail.com>
2024-08-29 15:10:17 +0300

    drop using spatie/backup, laravel octane, laravel-ide-helper packages

--- src/Console/Commands/PrepareCommand.php
+++ src/Console/Commands/PrepareCommand.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Process;
 
 class PrepareCommand extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'elattar:initialize {--basic}';
+    protected $description = 'This Command Used To Prepare Newly Created Laravel Project To Install all development tools';
     private string $workingDirectory;
 
     public function __construct()
@@ -15,14 +22,6 @@ class PrepareCommand extends Command
         parent::__construct();
         $this->workingDirectory = base_path();
     }
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'elattar:initialize {--basic}';
-
-    protected $description = 'This Command Used To Prepare Newly Created Laravel Project To Install all development tools';
 
     /**
      * Execute the console command.
@@ -38,12 +37,8 @@ class PrepareCommand extends Command
     {
         $basic = $this->option('basic');
 
-        if(! $basic)
-        {
-            $this->laravelOctane();
-            $this->ideHelper();
+        if (!$basic) {
             $this->logViewer();
-            $this->laravelBackup();
         }
 
         $this->installApi();
@@ -54,12 +49,10 @@ class PrepareCommand extends Command
         $this->info(Process::run('composer update --working-dir=' . $this->workingDirectory)->output());
     }
 
-    private function telescope(): void
+    private function logViewer(): void
     {
-        $this->info('Preparing Telescope .....');
-        $this->info(Process::run('composer require laravel/telescope --working-dir=' . $this->workingDirectory)->output());
-        $this->info(Process::run("php $this->workingDirectory/artisan telescope:install")->output());
-        $this->info(Process::run("php $this->workingDirectory/artisan migrate")->output());
+        $this->info('installing Log Viewer ....');
+        $this->info(Process::run('composer require opcodesio/log-viewer --dev --working-dir=' . $this->workingDirectory)->output());
     }
 
     public function installApi(): void
@@ -68,10 +61,12 @@ class PrepareCommand extends Command
         $this->info(Process::run("php $this->workingDirectory/artisan install:api")->output());
     }
 
-    private function ideHelper(): void
+    private function telescope(): void
     {
-        $this->info('Installing Ide Helper .....');
-        $this->info(Process::run('composer require --dev barryvdh/laravel-ide-helper --working-dir=' . $this->workingDirectory)->output());
+        $this->info('Preparing Telescope .....');
+        $this->info(Process::run('composer require laravel/telescope --working-dir=' . $this->workingDirectory)->output());
+        $this->info(Process::run("php $this->workingDirectory/artisan telescope:install")->output());
+        $this->info(Process::run("php $this->workingDirectory/artisan migrate")->output());
     }
 
     private function laravelModules(): void
@@ -86,33 +81,14 @@ class PrepareCommand extends Command
         $this->info(Process::run('composer require elattar/fast-paginate --working-dir=' . $this->workingDirectory)->output());
     }
 
-    public function laravelOctane(): void
-    {
-        $this->info('Installing Laravel Octane .....');
-        $this->info(Process::run('composer require laravel/octane --working-dir=' . $this->workingDirectory)->output());
-        $this->info(Process::run("php $this->workingDirectory/artisan octane:install --server=frankenphp")->output());
-    }
-
-    private function logViewer(): void
-    {
-        $this->info('installing Log Viewer ....');
-        $this->info(Process::run('composer require opcodesio/log-viewer --dev --working-dir=' . $this->workingDirectory)->output());
-    }
-
-    public function laravelBackup(): void
-    {
-        $this->info('Installing Laravel Backup .....');
-        $this->info(Process::run('composer require spatie/laravel-backup --working-dir=' . $this->workingDirectory)->output());
-    }
-
     private function composerChanges(): void
     {
         $this->info('Making Composer Changes....');
 
         Composer::start()
             ->push('autoload.psr-4.Modules\\', 'Modules/')
-            ->push('extra.merge-plugin.include',  ["Modules/*/composer.json"])
-            ->push('config.allow-plugins.wikimedia/composer-merge-plugin',  true)
+            ->push('extra.merge-plugin.include', ["Modules/*/composer.json"])
+            ->push('config.allow-plugins.wikimedia/composer-merge-plugin', true)
             ->write();
 
         $this->info('Reloading Composer....');
